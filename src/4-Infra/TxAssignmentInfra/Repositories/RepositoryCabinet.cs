ec3c34b2d9d65f58b3d0a6157fa6fb5c02a5d248
Fabio Carvalho <Fabio.Carvalho@sonicautomotive.com>
2023-12-07 19:58:38 -0300

    include the businness logic to register a cabinet

--- src/4-Infra/TxAssignmentInfra/Repositories/RepositoryCabinet.cs
+++ src/4-Infra/TxAssignmentInfra/Repositories/RepositoryCabinet.cs
@@ -1,12 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using StackExchange.Redis;
+using TxAssignmentInfra.Entities;
 
 namespace TxAssignmentInfra.Repositories
 {
-    public class RepositoryCabinet
+    public class RepositoryCabinet : IRepositoryCabinet
     {
+
+        private readonly IDatabase _database;
+
+        public RepositoryCabinet(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public async Task<RepositoryResponse> CreateCabinet(Cabinet cabinet)
+        {
+            try
+            {
+                var serializedProduct = JsonConvert.SerializeObject(cabinet);
+                await _database.StringSetAsync(cabinet.Id.ToString(), serializedProduct);
+                return new RepositoryResponse { Success = true, Message = "Cabinet created successfully." };
+            }
+            catch (Exception ex)
+            {
+                return new RepositoryResponse { Success = false, Message = ex.Message };
+            }
+        }
+
+        public async Task<RepositoryResponse> DeleteCabinet(Guid IdCabinet)
+        {
+            try
+            {
+                bool deleted = await _database.KeyDeleteAsync(IdCabinet.ToString());
+                return new RepositoryResponse
+                {
+                    Success = deleted,
+                    Message = deleted ? "Cabinet deleted successfully." : "Failed to delete cabinet."
+                };
+            }
+            catch (Exception ex)
+            {
+                return new RepositoryResponse { Success = false, Message = ex.Message };
+            }
+        }
+
+        public async Task<RepositoryResponse<Cabinet>> GetCabinetById(Guid IdCabinet)
+        {
+            try
+            {
+                var serializedCabinet = await _database.StringGetAsync(IdCabinet.ToString());
+
+                if (serializedCabinet.IsNullOrEmpty)
+                {
+                    return new RepositoryResponse<Cabinet> { Success = false, Message = "Cabinet not found." };
+
+                }
+
+                var cabinet = JsonConvert.DeserializeObject<Cabinet>(serializedCabinet);
+                return new RepositoryResponse<Cabinet> { Success = true, Data = cabinet };
+            }
+            catch (Exception ex)
+            {
+                return new RepositoryResponse<Cabinet> { Success = false, Message = ex.Message };
+            }
+        }
+
+        public async Task<RepositoryResponse> UpadteCabinet(Guid IdCabinet, Cabinet cabinet)
+        {
+            try
+            {
+                var key = IdCabinet.ToString();
+                if (!await _database.KeyExistsAsync(key))
+                {
+                    return new RepositoryResponse { Success = false, Message = "Cabinet not found." };
+                }
+
+                var serializedCabinet = JsonConvert.SerializeObject(cabinet);
+                bool updated = await _database.StringSetAsync(key, serializedCabinet);
+                return new RepositoryResponse
+                {
+                    Success = updated,
+                    Message = updated ? "Cabinet updated successfully." : "Failed to update cabinet."
+                };
+            }
+            catch (Exception ex)
+            {
+                return new RepositoryResponse { Success = false, Message = ex.Message };
+            }
+        }
+
+        public Task<RepositoryResponse<Cabinet>> GetEmptySpaceOnCabinet(Guid IdProduct)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
